c5bb79dedd20b514e9b2ee7fd229455ebcedea0d
APAC\vup8hc <phuc.vuhong@vn.bosch.com>
2025-10-06 11:10:07 +0700

    add conffeti animation

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
-import React, { Suspense } from 'react';
+import React, { Suspense, useState, useEffect } from 'react';
+import Confetti from 'react-confetti';
 import './App.css';
 import Navbar from './components/UI/Navbar';
 import Footer from './components/UI/Footer';
@@ -11,8 +12,20 @@ const LazyLoadedImage = React.lazy(() => import('./components/LazyLoadedImage'))
 const LazyLoadedVideo = React.lazy(() => import('./components/LazyLoadedVideo'));
 
 function App() {
+  const [windowSize, setWindowSize] = useState({ width: window.innerWidth, height: window.innerHeight });
+
+  useEffect(() => {
+    const handleResize = () => {
+      setWindowSize({ width: window.innerWidth, height: window.innerHeight });
+    };
+
+    window.addEventListener('resize', handleResize);
+    return () => window.removeEventListener('resize', handleResize);
+  }, []);
+
   return (
     <div className="App">
+      <Confetti width={windowSize.width} height={windowSize.height} />
       <Navbar />
       <main>
         <CelebrationMessage />
